febba78b6c46901557962bfc913733fed6527f48
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-11-19 17:18:43 +0100

    add item "Watch Youtube video" to the context menu

--- youtube-extension/src/browser/youtube-extension-contribution.ts
+++ youtube-extension/src/browser/youtube-extension-contribution.ts
@@ -1,10 +1,11 @@
 import { injectable, inject } from "inversify";
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry, MessageService } from "@theia/core/lib/common";
-import { CommonMenus } from "@theia/core/lib/browser";
+// import { DialogOverlayService } from "@theia/core/src/browser";
+import { EDITOR_CONTEXT_MENU } from '@theia/editor/lib/browser';
 
 export const YoutubeExtensionCommand = {
     id: 'YoutubeExtension.command',
-    label: "Say Hello"
+    label: "Watch Youtube video"
 };
 
 @injectable()
@@ -12,11 +13,13 @@ export class YoutubeExtensionCommandContribution implements CommandContribution 
 
     constructor(
         @inject(MessageService) private readonly messageService: MessageService,
+        // @inject(DialogOverlayService) private readonly dialogOverlayService: DialogOverlayService,
     ) { }
 
     registerCommands(registry: CommandRegistry): void {
         registry.registerCommand(YoutubeExtensionCommand, {
-            execute: () => this.messageService.info('Hello World!')
+            // execute: () => this.dialogOverlayService.initialize()
+            execute: () => this.messageService.info('WOOHOOO');
         });
     }
 }
@@ -25,7 +28,7 @@ export class YoutubeExtensionCommandContribution implements CommandContribution 
 export class YoutubeExtensionMenuContribution implements MenuContribution {
 
     registerMenus(menus: MenuModelRegistry): void {
-        menus.registerMenuAction(CommonMenus.EDIT_FIND, {
+        menus.registerMenuAction(EDITOR_CONTEXT_MENU, {
             commandId: YoutubeExtensionCommand.id,
             label: YoutubeExtensionCommand.label
         });
